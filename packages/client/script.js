d4f35e283fb1c1abcb7d7f15f2777de7e6869a54
Luzagar <luzagar.abonner@gmail.com>
2025-04-04 07:31:26 +0200

    fix settingstring

--- packages/client/script.js
+++ packages/client/script.js
@@ -707,7 +707,7 @@ var arrayOfSettingsItems = [
   'openMapCheckbox',
   'spinnerSpeedCheckbox',
   'openDotCheckbox',
-  'skipHcCheckBox',
+  'skipHcCheckbox',
   'optDungeonsCheckbox',
   'skipZantCheckbox',
   'hcShortcutCheckbox',
