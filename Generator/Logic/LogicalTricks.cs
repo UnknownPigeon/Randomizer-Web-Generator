c944a3181d24ea5e1cbcc06f7bb63a02dd58ecbf
lunarsoap5 <justindakotacarter@gmail.com>
2026-05-26 10:45:20 -0500

    Update trick naming and add/remove tricks

--- Generator/Logic/LogicalTricks.cs
+++ Generator/Logic/LogicalTricks.cs
@@ -33,7 +33,6 @@ namespace TPRandomizer
                 // Ordon Tricks
                 // Faron Tricks
                 { "Faron Mist Stump Chest as Wolf", "" },
-                { "Baba Serpent Grotto With Sword", "" },
                 { "Baba Serpent Grotto With Wolf", "" },
                 // Eldin Tricks
                 { "Death Mountain Geysers Shield Skip", "" },
@@ -42,14 +41,15 @@ namespace TPRandomizer
                 { "Eldin Lava Cave Wolf Jump to Bottom", "" },
                 { "Kakariko Village Watchtower Alcove Chest as Wolf", "" },
                 { "Kakariko Village Bomb Rock Spire Heart with Epona and Claw", "" },
-                { "Kakariko Village Rock Spire Heart Piece with Boomerang", "" },
+                { "Kakariko Village Bomb Rock Spire Heart with Boomerang", "" },
                 { "Kakariko Gorge Double Clawshot Chest as Wolf", "" },
                 { "Bridge of Eldin Owl Statue Chest as Wolf", "" },
                 { "Hidden Village Checks Without Clawshot", "" },
                 // Lanayru Tricks
-                { "Helmasaur Grotto with Clawshot", "" },
+                { "Helmasaur Grotto with Only Clawshot", "" },
                 { "Izas Helping Hand With Boomerang", "" },
                 { "Underwater Goron Without Zora Armor", "" },
+                { "Zoras Domain Underwater Rupees Without Zora Armor", "" },
                 { "Lake Lantern Cave Without Lantern", "llc_no_lantern" },
                 { "Outside Castle Town South Double Claw Chest With Wolf", "" },
                 { "Outside Castle Town South Double Claw Chest With Sword", "" },
@@ -60,9 +60,11 @@ namespace TPRandomizer
                 { "Star 1 Without Clawshot", "" },
                 { "Star 2 Without Double Clawshot", "" },
                 { "Water Toadpoli Grotto with Wolf", "" },
-                { "Zoras Domain Box Jump", "" },
+                { "Zoras Domain Waterfall Ledge With Box and Sword", "" },
+                { "Zoras Domain Wafterfall Ledge With Spinner", "" },
+                { "Legendary Hylian Loach With Frog Lure", "" },
                 // Desert Tricks
-                { "Bulblin Camp Boar with only Irons", "" },
+                { "Bulblin Camp Boar With Irons", "" },
                 // Snowpeak Tricks
                 // Forest Temple
                 { "Forest Temple Lobby Without Ranged Items", "" },
@@ -73,31 +75,31 @@ namespace TPRandomizer
                 { "Forest Temple Ook access with Midna", "" },
                 // Goron Mines
                 { "Defeat Fyrus Without Sword", "" },
-                { "Defeat Fyrus withow Bow", "" },
+                { "Defeat Fyrus Without Irons", "" },
                 // Lakebed Temple
                 { "Lakebed Temple Chandelier Drop", "" },
-                { "Lakebed Temple Central Room With Enemy LJAs", "" },
                 { "Defeat Morpheel Without Sword", "" },
                 { "Lakebed Temple Big Key Chest Without Bombs", "" },
                 { "Lakebed Temple Big Key Skip", "" },
                 // Arbiters Grounds
-                { "Arbiters Grounds Poe 1 Skip", "" },
-                { "Arbiters Grounds Big Key with Wolf", "" },
-                { "Arbiters Grounds Entrance Chain without Clawshot", "" },
+                { "Arbiters Grounds Pillar Jump", "" },
+                { "Arbiters Grounds Big Key With Wolf", "" },
+                { "Arbiters Grounds Entrance Chain Without Clawshot", "" },
                 // Snowpeak Ruins
                 { "Snowpeak Ruins Ladder Freezard Cancel", "" },
                 { "Snowpeak Ruins Northeast Chandelier Chest With Backslice", "" },
                 { "Snowpeak Ruins Lobby Chandelier Chest Without Wolf", "" },
                 // Temple of Time
-                { "Temple of Time Activate Crystal Switches With Clawshot", "" },
+                { "Temple of Time Crystal Switches With Clawshot", "" },
                 // City in the Sky
                 { "City in the Sky Fan Skip", "" },
-                { "City in the Sky Central Room With No Items", "" },
+                { "City in the Sky Central Occa Room With No Items", "" },
                 { "City in the Sky Entrance Crystal Switch with Ball and Chain", "" },
                 { "City in the Sky East Wing First Room with No Items", "" },
                 { "City in the Sky North Wing With Double Clawshots", "" },
                 { "City in the Sky Dinalfos Room with Clawshot", "" },
                 { "City in the Sky Central Outside Ledge Chest With Clawshot", "" },
+                { "City in the Sky Compass Chest With Double Clawshots", "" },
                 // Palace of Twilight
                 // Hyrule Castle
                 { "Hyrule Castle Painting Switch With Bombs", "" },
@@ -106,7 +108,7 @@ namespace TPRandomizer
                 { "Hyrule Castle Skip Main Hall Barrier", "" },
                 { "Hyrule Castle Chandeliers with Single Clawshot", "" },
                 { "Hyrule Castle Tower Climb with Single Clawshot", "" },
-                { "Defeat Dark Beast Ganon without Wolf", "" },
+                { "Defeat Dark Beast Ganon Without Wolf", "" },
             };
     }
 }
