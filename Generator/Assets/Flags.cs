d75d6f95e70684cfc0188023c46711e621ae685b
Luzagar <luzagar.abonner@gmail.com>
2026-08-10 23:23:18 +0200

    fix south faron portal

--- Generator/Assets/Flags.cs
+++ Generator/Assets/Flags.cs
@@ -23,7 +23,7 @@ namespace TPRandomizer.Assets
         {
             { 0x2, 0x46 }, // Midna jump 1 mist area.
             { 0x2, 0x47 }, // Midna jump 1 mist area.
-            { 0x2, 0x98 }, // South Faron Portal.
+            { 0x2, 0x51 }, // S warp shadow beast revive cs
         };
 
         /// <summary>
