7cc2f2660abae1b49cd1108259558382e27f817f
Korambu7 <xxleobestnessxx@gmail.com>
2026-09-08 10:30:42 +0200

    Remove useless code

--- Generator/Hints/HintCreator/BarrenHintCreator.cs
+++ Generator/Hints/HintCreator/BarrenHintCreator.cs
@@ -430,9 +430,6 @@ namespace TPRandomizer.Hints.HintCreator
                         // Pick all valid zones
                         foreach (KeyValuePair<string, string[]> pair in ZoneUtils.zoneNameToChecks)
                         {
-                            Console.WriteLine(pair.Key);
-                            if (pair.Key == "Fish Journal Zone")
-                                continue;
                             result.Add(AreaId.ZoneStr(pair.Key));
                         }
                         break;
